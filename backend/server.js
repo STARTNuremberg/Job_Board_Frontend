120be0aae32c1275e2f4dd1b77b7244339e5f4cb
CoKn <43541871+CoKn@users.noreply.github.com>
2023-07-03 16:20:27 +0200

    set up changes

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 const app = express()
 
 
-mongoose.connect("mongodb://localhost:27017/jobs", {
+mongoose.connect("mongodb://mongodb:27017/jobs", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
@@ -13,6 +13,5 @@ mongoose.connect("mongodb://localhost:27017/jobs", {
     .catch((error) => {
         console.error("Error connecting to MongoDB:", error);
     });
-
 
 app.listen(3080)
