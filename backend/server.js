56f2f6ad889c0e9849a7e69fcb83980aaa8d653d
CoKn <43541871+CoKn@users.noreply.github.com>
2023-07-06 15:07:48 +0200

    add dependencies

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const jobRouter =  require("./routes/jobs")
+
 const app = express()
 
 
-mongoose.connect("mongodb://mongodb:27017/jobs", {
+mongoose.connect("mongodb://db:27017/", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
@@ -13,5 +15,25 @@ mongoose.connect("mongodb://mongodb:27017/jobs", {
     .catch((error) => {
         console.error("Error connecting to MongoDB:", error);
     });
+
+
+app.use(express.urlencoded({extended: false}))
+
+app.get("/", (req, res) => {
+    const jobs = [{
+        title: "first job",
+        createdAt: new Date(),
+        description: "This is a awesome job description"
+    },
+        {
+            title: "second job",
+            createdAt: new Date(),
+            description: "This is a awesome job description"
+        }]
+    // res.render("jobs/index", {jobs: jobs})
+    res.send({jobs: jobs})
+})
+
+app.use('/jobs', jobRouter)
 
 app.listen(3080)
