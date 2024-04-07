972eb411599f96acaadc9b5f84746ddd90d66be0
AbderrahmaneBennani <revox2k@gmail.com>
2024-04-07 14:07:07 +0200

    root links

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const RootLayout = () => {
-  return <div>RootLayout</div>;
+  return <div>
+    <Link
+      to="/sign-in"
+      className="text-primary-500 text-small-semibold ml-1">
+      Login In
+    </Link>
+
+    <Link
+      to="/sign-up"
+      className="text-primary-500 text-small-semibold ml-1">
+      Sign Up
+    </Link>
+
+
+
+  </div>;
 };
 
 export default RootLayout;
