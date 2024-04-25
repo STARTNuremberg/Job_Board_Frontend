579521d866e4b53c5aa6246a8988508411a0e56d
AbderrahmaneBennani <revox2k@gmail.com>
2024-04-25 13:56:18 +0200

    install react-auth-kit

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import AuthProvider from "react-auth-kit/AuthProvider";
+import createStore from "react-auth-kit/createStore";
+
+const store = createStore({
+  authName: "_auth",
+  authType: "cookie",
+  cookieDomain: window.location.hostname,
+  cookieSecure: false,
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
+  <AuthProvider store={store}>  
   <BrowserRouter>
     <App />
   </BrowserRouter>
+  </AuthProvider>
 );
