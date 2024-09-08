5fd983e100ef2f0e9c5e52aef81b626a4238429b
AbderrahmaneBennani <revox2k@gmail.com>
2024-09-08 10:10:10 +0200

    edit Rootlayout

--- src/_root/RootLayout.tsx
+++ src/_root/RootLayout.tsx
@@ -1,17 +1,15 @@
-import React from "react";
-import { Link } from "react-router-dom";
 import { Outlet } from "react-router-dom";
-import { Toaster } from "@/components/ui/toaster"
+import { Toaster } from "@/components/ui/toaster";
+import Navbar from "@/components/Navbar.tsx";
 
 const RootLayout = () => {
-
-  return <div>
-    
-    <Outlet />
-    <Toaster />
-
-
-  </div>;
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+      <Toaster />
+    </div>
+  );
 };
 
 export default RootLayout;
