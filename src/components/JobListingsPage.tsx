5f37b3c26b763c87eafb2a1f8456f20d786df696
Kübra Demirhan <93667932+hopeful12@users.noreply.github.com>
2024-09-08 13:12:30 +0200

    Added JobPosting components

--- src/components/JobListingsPage.tsx
+++ src/components/JobListingsPage.tsx
@@ -25,6 +25,7 @@ interface JobPosting {
 }
 const JobListingsPage = () => {
   const [jobPostings, setJobPostings] = useState<JobPosting[]>([]);
+  const [currentJob, setCurrentJob] = useState(0);
   useEffect(() => {
     fetch(url)
       .then((res) => {
@@ -35,13 +36,57 @@ const JobListingsPage = () => {
         setJobPostings(data);
       });
   }, []);
+
   console.log(jobPostings);
   return (
-    <div className="flex h-screen w-screen justify-center ">
-      {jobPostings.map((job) => (
-        <p key={job.id}>{job.title}</p>
-      ))}
-    </div>
+    <>
+      <div className="w-full h-24 border-2">Placeholder Navbar</div>
+      <div className="w-full h-24 border-2 flex justify-center items-center gap-6">
+        <div className="w-1/3 ">
+          <input
+            className="border-2 w-1/2 p-2 "
+            type="text"
+            placeholder="Search"
+          ></input>
+          <input
+            className="border-2 w-1/2 p-2"
+            type="text"
+            placeholder="Location"
+          ></input>
+        </div>
+        <button
+          className=" border-3 p-3 bg-blue-950 text-white  text-center"
+          type="submit"
+        >
+          Search
+        </button>
+      </div>
+      <div className="flex m-auto w-full h-content items-center justify-center">
+        <ul className="w-1/3  ">
+          {jobPostings.map((job) => (
+            <li
+              key={job.id}
+              className="w-full h-1/2 border-2  p-8 border-r-10 list-none "
+            >
+              <a className="text-2xl font-bold hover:underline ">
+                {job.title} (m/w/d)
+              </a>
+
+              <p className="">Company {job.company}</p>
+              <p>{job.description}</p>
+              <p>{job.location}</p>
+              <p className="bg-slate-300 w-20 h-5 opacity-50 ">
+                {JobTypeEnum[job.job_type]}
+              </p>
+            </li>
+          ))}
+        </ul>
+        <div className="w-1/3 h-1/2 border-2 m-5">
+          <h1>Job Title</h1>
+          <p>Job Description</p>
+        </div>
+      </div>
+    </>
   );
 };
 
