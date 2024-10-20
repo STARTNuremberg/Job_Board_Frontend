01af7f33aab212cede627146a7c0f39ffc703d4d
Abderrahmane Bennani <93011714+AbderrahmaneBennani@users.noreply.github.com>
2024-10-20 10:12:06 +0200

    fetch Userdata and display Profile

--- src/_root/pages/Profile.tsx
+++ src/_root/pages/Profile.tsx
@@ -1,5 +1,37 @@
+import React, { useState } from "react";
+import useIsAuthenticated from "react-auth-kit/hooks/useIsAuthenticated";
+import useAuthUser from "react-auth-kit/hooks/useAuthUser";
+import axios from "axios";
+
 const Profile = () => {
-  return <div className="h-screen w-screen">My Profile</div>;
+  const isAuthenticated = useIsAuthenticated();
+  const auth = useAuthUser();
+  const [user, setUser] = useState({ username: "", email: "", avatar: "" });
+
+  axios
+    .get(`http://localhost:8000/users/user/`, {
+      headers: {
+        accept: "*/*",
+        Authorization: `Bearer ${auth.token}`,
+      },
+    })
+    .then((res) => {
+      setUser({
+        username: res.data.username,
+        email: res.data.email,
+        avatar: res.data.avatar,
+      });
+    });
+  return (
+    <>
+      {isAuthenticated && (
+        <div className="h-screen w-screen">
+          <h1>Hello {user.username}</h1>
+          <h2>Your Email: {user.email}</h2>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Profile;
